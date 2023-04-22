2ce9d41299077e1623d5fb3580e85426b3ecbde0
shah18122 <60107981+shahsmit18122@users.noreply.github.com>
2023-04-22 22:19:27 +0530

    Add files via upload

---
+++ record_users.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="attendance_admin",
+  passwd="Admin",
+  database="attendance_system"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522()
+
+try:
+  while True:
+    print('Place Card to record attendance')
+    id, text = reader.read()
+    cursor.execute("SELECT id FROM users WHERE rfid_uid="+str(id))
+    cursor.fetchone()
+
+    if cursor.rowcount >= 1:
+      print("Overwrite existing user?")
+      overwrite = input("Overwite (Y/N)? ")
+      if overwrite[0] == 'Y' or overwrite[0] == 'y':
+        print("Overwriting user.")
+        time.sleep (1)
+        sql_insert = "UPDATE users SET name = %s WHERE rfid_uid=%s"
+      else:
+        continue;
+    else:
+      sql_insert = "INSERT INTO users (name, rfid_uid) VALUES (%s, %s)"
+
+    print('Enter new name')
+    new_name = input("Name: ")
+
+    cursor.execute(sql_insert, (new_name,id))
+
+    db.commit()
+
+    print("User :" + new_name + " Saved")
+    time.sleep(2)
+finally:
+  GPIO.cleanup()
